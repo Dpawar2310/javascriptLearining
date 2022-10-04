bb699b9c1f9c8a6b619518796413300a7dc1bffd
Dpawar2310 <dipalisanpawar@gmail.com>
2022-10-04 17:54:33 +0100

    First program execution in JS
    First program execution in JS

---
+++ demo.js
@@ -0,0 +1,6 @@
+console.log("Dipali Aravind pawar");
+console.log("My Hobbies- 1.Reading 2.coocking 3.traveling");
+console.info("My Strength- 1.hardwork 2.Honesty and loyalty 3.Positive thinker");
+console.warn("My Weakness- 1.Emotional 2.presentation skill 3.sensitive");
+console.log(30+50);
+console.log(49*479)
