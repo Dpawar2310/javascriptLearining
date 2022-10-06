a9b68e654e9617a19c6a3dc8fde81fb920d68a82
Dpawar2310 <dipalisanpawar@gmail.com>
2022-10-06 19:44:00 +0100

    Create 01_swap_var_assign.js

---
+++ 01_swap_var_assign.js
@@ -0,0 +1,21 @@
+var sweety="sweety";
+var cutie="cutie";
+var temp="sweety";
+sweety=cutie;
+cutie=temp;
+console.log("Before Swap");
+console.log(sweety, cutie);
+console.log("After Swap");
+console.log(cutie,sweety);
+
+var num1=100;
+var num2=200;
+var num3=300;
+console.log("Before Swap");
+console.log(num1,num2,num3);
+var temp=num1;
+num1=num2;
+num2=num3;
+num3=temp;
+console.log("After Swap");
+console.log(num1,num2,num3);
