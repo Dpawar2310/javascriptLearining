ff25a995f644eb22524217428b8f84c68c96b691
Dpawar2310 <dipalisanpawar@gmail.com>
2022-10-06 19:44:12 +0100

    Create 01_variable_assign.js

---
+++ 01_variable_assign.js
@@ -0,0 +1,25 @@
+var myFullName;
+console.log(myFullName);
+myFullName="Dipali Aravind Pawar"
+console.log(myFullName);
+
+var myLove;
+myLove="I love only JS";
+console.log(myLove);
+
+var firstName ="Dipali";
+var lastName ="Pawar";
+var age ="26";
+var address ="Islampur";
+var pin_code ="415409";
+var gender ="Female";
+var is_married ="No";
+console.log(firstName);
+console.log(lastName);
+console.log(age);
+console.log(address);
+console.log(pin_code);
+console.log(gender);
+console.log(is_married);
+
+
