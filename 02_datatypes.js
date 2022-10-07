b9f10c2e602169ab56e6b159c5d81ccd3b9d6281
Dpawar2310 <dipalisanpawar@gmail.com>
2022-10-07 19:19:15 +0100

    Create 02_datatypes.js

---
+++ 02_datatypes.js
@@ -0,0 +1,18 @@
+//check the different type of datatype
+var pin=411056;
+console.log(typeof pin);
+console.log("------------------------------");
+var fname="Dipali";
+console.log(typeof fname);
+console.log("------------------------------");
+var married_status=false;
+console.log(typeof married_status);
+console.log("-------------------------------");
+
+//check the value id null or undefined
+var first_name;
+console.log(typeof first_name);
+console.log("------------------------------");
+var age;
+console.log(typeof age);
+console.log("------------------------------");
