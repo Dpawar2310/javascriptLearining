30fb1858b205c392be27884dde7aa8b818c9662f
Dpawar2310 <dipalisanpawar@gmail.com>
2022-10-11 22:20:11 +0100

    string.is
    string.is

---
+++ 04_string.js
@@ -0,0 +1,13 @@
+console.log("============ Que1.=================");
+str = "My Dream Company is Infosys";
+console.log(str);
+
+console.log("===========Que2.====================");
+var hobbies1 ="Reading";
+var hobbies2 ="Coocking";
+var hobbies3 ="Travelling";
+console.log(hobbies1);
+console.log(hobbies2);
+console.log(hobbies3);
+console.log("==========concat()===============");
+console.log(hobbies1.concat("   ", hobbies2 ,"  ", hobbies3));
