158ce2732c59ff7d9bc429861a3b9886c879dc40
Dpawar2310 <dipalisanpawar@gmail.com>
2022-10-14 18:37:30 +0100

    Create 06_switch_stat.js

---
+++ 06_switch_stat.js
@@ -0,0 +1,24 @@
+var switchCase = 2;
+switch (switchCase) {
+    case 1:
+        console.log("Monday");
+        break;
+    case 2:
+        console.log("Tuesday");
+        break;
+    case 3:
+        console.log("Wenesday");
+        break;
+    case 4:
+        console.log("Thursday");
+        break;
+    case 5:
+        console.log("Saturday");
+        break;
+    case 6:
+        console.log("Sunday");
+        break;
+    default:
+        console.log(`Invalid input`);
+        break
+}
