5c597e37518e31bc2f2b1b14d7e8d554a1232f31
Dpawar2310 <dipalisanpawar@gmail.com>
2022-10-16 20:38:43 +0100

    calculator.html
    calculator.html

--- demo.js
+++ demo.js
@@ -4,3 +4,5 @@ console.info("My Strength- 1.hardwork 2.Honesty and loyalty 3.Positive thinker")
 console.warn("My Weakness- 1.Emotional 2.presentation skill 3.sensitive");
 console.log(30+50);
 console.log(49*479)
+
+
