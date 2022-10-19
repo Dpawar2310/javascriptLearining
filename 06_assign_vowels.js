db50ea8c52a23393a41857253081bf820d6c60aa
Dpawar2310 <dipalisanpawar@gmail.com>
2022-10-19 21:29:10 +0100

    Create 06_assign_vowels.js

---
+++ 06_assign_vowels.js
@@ -0,0 +1,9 @@
+var str = "Good Morning IT champ";
+var lowerCase = str.toLowerCase();
+var lowerCaseVowels = lowerCase.includes(char)
+for (let index = 0; index < str.length; index++) {
+    var char = str.charAt(index);
+    if (char) {
+        console.log(char);
+    }
+}
