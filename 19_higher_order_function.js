f5881a835a3483530f168f6319f4843f110c4aad
Dpawar2310 <dipalisanpawar@gmail.com>
2022-11-04 21:49:06 +0530

    Create 19_higher_order_function.js

---
+++ 19_higher_order_function.js
@@ -0,0 +1,27 @@
+function do_assignment(callback) {
+    console.log("I am Doing assignment and it is done now...");
+    callback();
+    return function() {
+        console.log("Yahooo.. mai to ban gaya....");
+    }
+}
+function copy_assignment() {
+    console.log("Hey bro.. Thank you. Let me copy");
+}
+
+let returnFun = do_assignment(copy_assignment);
+returnFun();
+
+console.log('==========================================');
+function outer(calling) {
+    console.log('outer fucntion');
+    calling();
+    return function(){
+        console.log('return fucntion');
+    }
+}
+function inner() {
+    console.log('inner');
+    
+}
+outer(inner)();
