c92d9e1da038b759e3cecc52d251064c0b537ad1
Dpawar2310 <dipalisanpawar@gmail.com>
2022-11-04 21:49:35 +0530

    Create 23_arrow_function.js

---
+++ 23_arrow_function.js
@@ -0,0 +1,36 @@
+let sum = (x,y)=>{
+    return x+y;
+    }
+    console.log(sum(140,15));
+    
+    
+    let mult = ()=>{
+    
+    }
+    mult();
+    
+    
+    
+    function display(){
+        console.log("Hello");
+    }
+    
+    let show =  () => {
+        console.log("Hello");
+    }
+    show();
+    
+    
+    let add = function(n1, n2) {
+        return n1 + n2;
+    }
+    console.log( add(10, 20));
+    
+    let sums = (n1, n2) =>  n1 + n2; 
+    
+    // let sum1 = (n1, n2) => {
+    //     return n1 + n2
+    // } 
+    
+    console.log( sums(10, 20));
+    
